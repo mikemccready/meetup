227c516621e627bf643432e8a6186ca09614892d
Mike McCready <michaeljmccready@gmail.com>
2016-04-27 19:43:55 -0700

    rework gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,35 +3,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var minifycss = require('gulp-minify-css');
-var livereload = require('gulp-livereload');
-
-gulp.task('express', function(){
-	var express = require('express');
-	var app = express();
-	app.use(express.static('./public'));
-	app.listen(8080, '0.0.0.0');
-});
-
-var sassOptions = {
-  errLogToConsole: true,
-  outputStyle: 'expanded'
-};
 
 gulp.task('sass', function() {
-  	return gulp.src('./sass/*.scss')
-  		.pipe(sass(sassOptions).on('error', sass.logError))
-  		.pipe(autoprefixer())
-  		.pipe(minifycss())
-  		.pipe(gulp.dest('./public/css'))
-  		.pipe(livereload());
+  gulp.src('sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./public/css'))
 });
 
-gulp.task('watch', function() {
-	livereload.listen();
-	return gulp.watch('./sass/*.scss', ['sass']);
-});
-
-gulp.task('default', ['express', 'sass', 'watch'], function() {
+gulp.task('default', function() {
 
 });
